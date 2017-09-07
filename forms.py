27391aa2b2cbe89f2e1dd8e37c27615f1e469734
Robert Dickerson <rdickeriii@gmail.com>
2017-09-07 13:00:32 -0700

    Refactor: Adding Validators
    end_of_line

--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 class SignupForm(Form):
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    email = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Sign Up')
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up', validators=[DataRequired()])
