5759f7ec2e6de0b3cc8e07d20eccac7bc4a47156
Robert Dickerson <rdickeriii@gmail.com>
2017-09-07 12:28:12 -0700

    Refactor: Ugh...
    end_of_line

--- routes.py
+++ routes.py
@@ -23,7 +23,7 @@ def about():
 @app.route("/signup")
 def signup():
     form = SignupForm()
-    return render_template("signup.html", form=form)
+    return render_template('signup.html', form=form)
 
 
 if __name__ == "__main__":
