56037825059dc5e18778e4ed8a37de16bbb73e0c
Robert Dickerson <rdickeriii@gmail.com>
2017-09-07 13:04:23 -0700

    Refactor: Added custom error messages
    end_of_line

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class SignupForm(Form):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last Name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password.")])
     submit = SubmitField('Sign Up', validators=[DataRequired()])
